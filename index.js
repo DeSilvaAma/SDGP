196fbe01d664ec28c5b549db684696a9844897cc
DeSilvaAma <desilvaama2002@gmail.com>
2024-01-25 11:28:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "aid_express"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO user (userId, name, age, adress) VALUES (123456789, 'Test Name', 20, 'Test Address')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
